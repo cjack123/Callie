623dae3b789ba615f14831b93eeb1050bc0c1c03
Chaneice Jackson <c.jackson0126@gmail.com>
2023-02-17 11:40:49 -0600

    styling donw

--- src/Callie.js
+++ src/Callie.js
@@ -140,22 +140,22 @@ function Callie() {
       <div className="output">
         <div className="previous-operand">{formatOperand(previousOperand)} {operation}</div>
         <div className="current-operand">{formatOperand(currentOperand)}</div>
-        </div>
+      </div>
         <button className="span-two" onClick={() => dispatch({ type: ACTIONS.CLEAR})}>AC</button>
         <button onClick={() => dispatch({ type: ACTIONS.DELETE})}>DEL</button>
-        <OperationButton operation="÷" dispatch={dispatch} />
+        <OperationButton operation="÷" dispatch={dispatch} className="operation" />
         <DigitButton digit="1" dispatch={dispatch} />
         <DigitButton digit="2" dispatch={dispatch} />
         <DigitButton digit="3" dispatch={dispatch} />
-        <OperationButton operation="*" dispatch={dispatch} />
+        <OperationButton operation="*" dispatch={dispatch} className="operation" />
         <DigitButton digit="4" dispatch={dispatch} />
         <DigitButton digit="5" dispatch={dispatch} />
         <DigitButton digit="6" dispatch={dispatch} />
-        <OperationButton operation="+" dispatch={dispatch} />
+        <OperationButton operation="+" dispatch={dispatch} className="operation" />
         <DigitButton digit="7" dispatch={dispatch} />
         <DigitButton digit="8" dispatch={dispatch} />
         <DigitButton digit="9" dispatch={dispatch} />
-        <OperationButton operation="-" dispatch={dispatch} />
+        <OperationButton operation="-" dispatch={dispatch} className="operation" />
         <DigitButton digit="." dispatch={dispatch} />
         <DigitButton digit="0" dispatch={dispatch} />
         <button className="span-two" onClick={() => dispatch({ type: ACTIONS.EVALUATE})}>=</button>
